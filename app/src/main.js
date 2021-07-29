f98788c10ab0959aaca0bfcc0ff0117b18804fb8
KingDiegoA <d.sanmartinn10@gmail.com>
2021-07-29 17:55:16 -0400

    fix

--- app/src/main.js
+++ app/src/main.js
@@ -13,7 +13,11 @@ import crearMiembro from './components/CrearMiembros';
 import listarMiembros from './components/ListarMiembros';
 import modificarMiembro from './components/ModificarMiembros';
 import contacto from './components/Contacto';
-
+import login from './components/Login';
+import horas from './components/Horas';
+import modificarHoras from './components/ModificarHoras';
+import generarHoras from './components/Calendarizar';
+import estadisticas from './components/Estadisticas';
 
 //Creacion de los componentes
 Vue.component('inicio', inicio);
@@ -21,7 +25,11 @@ Vue.component('listarMiembros', listarMiembros);
 Vue.component('crearMiembro', crearMiembro);
 Vue.component('modificarMiembro', modificarMiembro);
 Vue.component('contacto', contacto);
-
+Vue.component('login', login);
+Vue.component('horas', horas);
+Vue.component('modificarHoras', modificarHoras);
+Vue.component('generarHoras', generarHoras);
+Vue.component('estadisticas', estadisticas);
 
 //Uso de Vue-Router
 Vue.use(VueRouter);
@@ -32,8 +40,13 @@ const routes = [
   {path:'/inicio', component:inicio},
   {path:'/miembros', component:listarMiembros},
   {path:'/crear', component:crearMiembro, name:'crearMiembro'},
-  {path:'/editar/:id', component:modificarMiembro, name:'modificarMiembro'},
+  {path:'/EditarMiembro/:id', component:modificarMiembro, name:'modificarMiembro'},
+  {path:'/ModificarDatosHoras/:id', component:modificarHoras, name:'modificarHoras'},
+  {path:'/AgregarHoras/:id', component:generarHoras, name:'generarHoras'},
   {path:'/contacto/', component:contacto},
+  {path:'/login/', component:login},
+  {path:'/horas/', component:horas},
+  {path:'/estadisticas/', component:estadisticas},
 ]
 //Creacion del Objeto
 const router = new VueRouter({
